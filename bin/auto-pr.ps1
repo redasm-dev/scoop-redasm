3db68e5257dfb410512e7aa66fcab6a3a01fe256
Antonio Davide <contact@antoniodavide.dev>
2026-09-06 12:06:03 +0200

    fix: auto-pr.ps1 quoting

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "redasm-dev/scoop-redasm:master"
+    [String]$upstream = 'redasm-dev/scoop-redasm:master'
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
